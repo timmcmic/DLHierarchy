2d2541094468d082ebfde9396aab1f8aeb661aa0
Timothy McMichael <timmcmic@microsoft.com>
2024-02-16 16:55:12 -0500

    UPdating

--- Get-GroupWithChildren.ps1
+++ Get-GroupWithChildren.ps1
@@ -245,7 +245,7 @@ Function Get-GroupWithChildren()
                 out-logfile -string "Processing child..."
                 out-logfile -string $child.id
                 $childGroupIDs = New-Object System.Collections.Generic.HashSet[string] $processedGroupIds
-                $childNode = Get-GroupWithChildren -objectID $child.id -processedGroupIds $childGroupIDs -objectType $child.additionalProperties["@odata.type"] -queryMethodGraph:$true
+                $childNode = Get-GroupWithChildren -objectID $child.id -processedGroupIds $childGroupIDs -objectType $child.additionalProperties["@odata.type"] -queryMethodGraph:$true -expandGroupMembership $expandGroupMembership
                 $childNodes += $childNode
             }
         }
