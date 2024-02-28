ae918be3e47242b3845ae913fb9d1ccafdc1e404
Timothy McMichael <timmcmic@microsoft.com>
2024-02-27 20:48:04 -0500

    Updating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -15,25 +15,64 @@ function start-HTMLOutput
 
     out-logfile -string $functionHTMLFile
 
-    New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
-        New-HTMLTree -Checkbox none {
-            New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
-            New-HTMLTreeNode -Title 'Power comes on' {
-                New-HTMLTreeNode -Title 'Live screen' {
-                    New-HTMLTreeNode -Title 'New build'
-                    New-HTMLTreeNode -Title '<b>No</b> new build' {
-                        New-HTMLTreeNode -Title 'Need two tries to boot' {
-                            New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
+    if ($outputType -eq $functionMSGraphType)
+    {
+        <#
+        $string = $node.object.displayName +" (ObjectID: "+$node.object.id+") ("+$node.object.getType().name+")"
+
+        out-logfile -string  (("-" * $indent) + $string)
+
+        $global:outputFile += (("-" * $indent) + $string +"`n")
+
+        foreach ($child in $node.Children)
+        {
+            Print-Tree -node $child -indent ($indent + 2) -outputType $functionMSGraphType
+        }
+        #>
+    }
+    elseif ($outputType -eq $functionExchangeOnlineType)
+    {
+        New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
+            New-HTMLTree -Checkbox none {
+                New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
+                New-HTMLTreeNode -Title $groupObjectID {
+                    New-HTMLTreeNode -Title 'Live screen' {
+                        New-HTMLTreeNode -Title 'New build'
+                        New-HTMLTreeNode -Title '<b>No</b> new build' {
+                            New-HTMLTreeNode -Title 'Need two tries to boot' {
+                                New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
+                            }
+                            New-HTMLTreeNode -Title 'Does not need two tries to boot'
                         }
-                        New-HTMLTreeNode -Title 'Does not need two tries to boot'
+                    }
+                    New-HTMLTreeNode -Title "<b>No</b> live screen" {
+                        New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
                     }
                 }
-                New-HTMLTreeNode -Title "<b>No</b> live screen" {
-                    New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
-                }
-            }
-        } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
-    } -Online -ShowHTML
+            } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
+        } -Online -ShowHTML
+        $string = $node.object.displayName +" (ExchangeObjectID: "+$node.object.ExchangeObjectID+") ("+$node.object.recipientType+"/"+$node.object.recipientTypeDetails+")"
+
+        foreach ($child in $node.Children)
+        {
+            Print-Tree -node $child -indent ($indent + 2) -outputType $functionExchangeOnlineType
+        }
+    }
+    elseif ($outputType -eq $functionLDAPType)
+    {
+        <#
+        $string = $node.object.DisplayName +" (ObjectGUID:"+$node.object.objectGUID+") ("+$node.object.objectClass+")"
+        
+        out-logfile -string  (("-" * $indent) + $string)
+
+        $global:outputFile += (("-" * $indent) + $string +"`n")
+
+        foreach ($child in $node.Children)
+        {
+            Print-Tree -node $child -indent ($indent + 2) -outputType $functionLDAPType
+        }
+        #>
+    }
 
     <#
 
