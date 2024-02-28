4521605f07d32c0b8f8117f0708c0de93c70d1bf
Timothy McMichael <timmcmic@microsoft.com>
2024-02-28 14:52:20 -0500

    Updating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -83,10 +83,11 @@ function start-HTMLOutput
 
     out-logfile -string $functionHTMLFile
 
+    $string = $node.object.displayName +" (ExchangeObjectID: "+$node.object.ExchangeObjectID+") ("+$node.object.recipientType+"/"+$node.object.recipientTypeDetails+")"
+
         New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
         New-HTMLTree -Checkbox none {
             New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
-            $string = $node.object.displayName +" (ExchangeObjectID: "+$node.object.ExchangeObjectID+") ("+$node.object.recipientType+"/"+$node.object.recipientTypeDetails+")"
             new-treeNode -title $string -chilren {New-HTMLTreeChildNodes -node $node -outputType $functionExchangeOnlineType}
         } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
     } -Online -ShowHTML
