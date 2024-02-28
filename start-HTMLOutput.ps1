3f750f3a220c926b4abd21b400a3874324889238
Timothy McMichael <timmcmic@microsoft.com>
2024-02-28 13:45:17 -0500

    Updating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -39,27 +39,14 @@ function start-HTMLOutput
         {
             $string = $node.object.displayName +" (ExchangeObjectID: "+$node.object.ExchangeObjectID+") ("+$node.object.recipientType+"/"+$node.object.recipientTypeDetails+")"
 
-            New-HTMLTreeNode -Title $string {
-
-                foreach ($child in $node.Children)
-                {
-                    New-HTMLTreeChildNodes -node $node -outputType $functionExchangeOnlineType
-                }
-                <#
-                New-HTMLTreeNode -Title 'Live screen' {
-                    New-HTMLTreeNode -Title 'New build'
-                    New-HTMLTreeNode -Title '<b>No</b> new build' {
-                        New-HTMLTreeNode -Title 'Need two tries to boot' {
-                            New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
-                        }
-                        New-HTMLTreeNode -Title 'Does not need two tries to boot'
+            foreach ($child in $node.children)
+            {
+                New-HTMLTreeNode -Title $string {
+                        New-HTMLTreeChildNodes -node $node -outputType $functionExchangeOnlineType
                     }
-                }
-                New-HTMLTreeNode -Title "<b>No</b> live screen" {
-                    New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
-                }
-                #>
             }
+
+           
             <#
             out-logfile -string $node
             out-logfile -string $node.object.displayName
