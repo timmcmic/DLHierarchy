87d0157781326dd1d6953ba77e8da65f024a6654
Timothy McMichael <timmcmic@microsoft.com>
2024-02-28 13:27:53 -0500

    Updating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -9,7 +9,7 @@ function start-HTMLOutput
         $groupObjectID
     )
 
-    function New-HTMLTreeFileNodes 
+    function New-HTMLTreeChildNodes 
     {
         param(
         $node,
@@ -37,6 +37,21 @@ function start-HTMLOutput
         }
         elseif ($outputType -eq $functionExchangeOnlineType)
         {
+            New-HTMLTreeNode -Title 'Power comes on' {
+                New-HTMLTreeNode -Title 'Live screen' {
+                    New-HTMLTreeNode -Title 'New build'
+                    New-HTMLTreeNode -Title '<b>No</b> new build' {
+                        New-HTMLTreeNode -Title 'Need two tries to boot' {
+                            New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
+                        }
+                        New-HTMLTreeNode -Title 'Does not need two tries to boot'
+                    }
+                }
+                New-HTMLTreeNode -Title "<b>No</b> live screen" {
+                    New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
+                }
+            }
+            <#
             out-logfile -string $node
             out-logfile -string $node.object.displayName
             $string = $node.object.displayName +" (ExchangeObjectID: "+$node.object.ExchangeObjectID+") ("+$node.object.recipientType+"/"+$node.object.recipientTypeDetails+")"
@@ -46,6 +61,7 @@ function start-HTMLOutput
             {
                 New-HTMLTreeNode -Title $string -children {New-HTMLTreeFileNodes -node $child -outputType $functionExchangeOnlineType}
             }
+            #>
         }
         elseif ($outputType -eq $functionLDAPType)
         {
@@ -66,26 +82,16 @@ function start-HTMLOutput
 
     $functionHTMLSuffix = "html"
     $functionHTMLFile = $global:LogFile.replace("log","$functionHTMLSuffix")
+    $functionMSGraphType = "MSGraph"
+    $functionExchangeOnlineType = "ExchangeOnline"
+    $functionLDAPType = "LDAP"
 
     out-logfile -string $functionHTMLFile
 
     New-HTML -TitleText 'This is a test' -FilePath $functionHTMLFile {
         New-HTMLTree -Checkbox none {
             New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
-            New-HTMLTreeNode -Title 'Power comes on' {
-                New-HTMLTreeNode -Title 'Live screen' {
-                    New-HTMLTreeNode -Title 'New build'
-                    New-HTMLTreeNode -Title '<b>No</b> new build' {
-                        New-HTMLTreeNode -Title 'Need two tries to boot' {
-                            New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
-                        }
-                        New-HTMLTreeNode -Title 'Does not need two tries to boot'
-                    }
-                }
-                New-HTMLTreeNode -Title "<b>No</b> live screen" {
-                    New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
-                }
-            }
+            New-HTMLTreeChildNodes -node $node -outputType $functionExchangeOnlineType
         } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
     } -Online -ShowHTML
     <#
