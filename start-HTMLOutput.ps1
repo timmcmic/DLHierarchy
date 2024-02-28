b766d3d19327db172d20da03667916c663d939ac
Timothy McMichael <timmcmic@microsoft.com>
2024-02-27 21:15:15 -0500

    UPdating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -9,15 +9,74 @@ function start-HTMLOutput
         $groupObjectID
     )
 
+    function New-HTMLTreeFileNodes 
+    {
 
-    $functionMSGraphType = "MSGraph"
-    $functionExchangeOnlineType = "ExchangeOnline"
-    $functionLDAPType = "LDAP"
+        param(
+        $node,
+        $outputType
+        )
+
+        $functionMSGraphType = "MSGraph"
+        $functionExchangeOnlineType = "ExchangeOnline"
+        $functionLDAPType = "LDAP"
+
+        if ($outputType -eq $functionMSGraphType)
+        {
+            <#
+            $string = $node.object.displayName +" (ObjectID: "+$node.object.id+") ("+$node.object.getType().name+")"
+
+            out-logfile -string  (("-" * $indent) + $string)
+
+            $global:outputFile += (("-" * $indent) + $string +"`n")
+
+            foreach ($child in $node.Children)
+            {
+                Print-Tree -node $child -indent ($indent + 2) -outputType $functionMSGraphType
+            }
+            #>
+        }
+        elseif ($outputType -eq $functionExchangeOnlineType)
+        {
+            $string = $node.object.displayName +" (ObjectID: "+$node.object.id+") ("+$node.object.getType().name+")"
+
+            New-HTMLTreeNode -Title $string
+
+            foreach ($child in $node.Children)
+            {
+                New-HTMLTreeNode -node $child -outputType $functionExchangeOnlineType
+            }
+        }
+        elseif ($outputType -eq $functionLDAPType)
+        {
+            <#
+            $string = $node.object.DisplayName +" (ObjectGUID:"+$node.object.objectGUID+") ("+$node.object.objectClass+")"
+            
+            out-logfile -string  (("-" * $indent) + $string)
+
+            $global:outputFile += (("-" * $indent) + $string +"`n")
+
+            foreach ($child in $node.Children)
+            {
+                Print-Tree -node $child -indent ($indent + 2) -outputType $functionLDAPType
+            }
+            #>
+        }
+    }
+
     $functionHTMLSuffix = "html"
     $functionHTMLFile = $global:LogFile.replace("log","$functionHTMLSuffix")
 
     out-logfile -string $functionHTMLFile
-    out-logfile -string $outputType
+
+    New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
+        New-HTMLTree -Checkbox none {
+            New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
+            New-HTMLTreeNode -Title $groupObjectID {
+                New-HTMLTreeFileNodes -node $node -outputType $outputType
+            }
+        } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
+    } -Online -ShowHTML
 
     if ($outputType -eq $functionMSGraphType)
     {
@@ -36,26 +95,7 @@ function start-HTMLOutput
     }
     elseif ($outputType -eq $functionExchangeOnlineType)
     {
-        out-logfile -string "here"
-        New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
-            New-HTMLTree -Checkbox none {
-                New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
-                New-HTMLTreeNode -Title $groupObjectID {
-                    New-HTMLTreeNode -Title 'Live screen' {
-                        New-HTMLTreeNode -Title 'New build'
-                        New-HTMLTreeNode -Title '<b>No</b> new build' {
-                            New-HTMLTreeNode -Title 'Need two tries to boot' {
-                                New-HTMLTreeNode -Title 'Premature power_good signal. Try different power supply.' -Icon 'https://cdn-icons-png.flaticon.com/512/6897/6897039.png'
-                            }
-                            New-HTMLTreeNode -Title 'Does not need two tries to boot'
-                        }
-                    }
-                    New-HTMLTreeNode -Title "<b>No</b> live screen" {
-                        New-HTMLTreeNode -Title "Proceed to video failure chart" -Icon 'https://cdn-icons-png.flaticon.com/512/1294/1294758.png'
-                    }
-                }
-            } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1
-        } -Online -ShowHTML
+        
     }
     elseif ($outputType -eq $functionLDAPType)
     {
