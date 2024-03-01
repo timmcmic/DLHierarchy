e4dbe5d8fc4a7fba7c2e228f84ee5f0c6d1ef62f
Timothy McMichael <timmcmic@microsoft.com>
2024-03-01 10:25:53 -0500

    Updating

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -197,7 +197,6 @@ function start-HTMLOutput
         out-logfile -string ("Prcessing HTML: "+$string)
 
         New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
-            new-htmlText -text @(("Hierarchy output for group ID: "+$groupObjectID))
             New-HTMLTree -Checkbox none {
                 New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
                 New-HTMLTreeNode -title $string -children {New-HTMLTreeChildNodes -node $node -outputType $functionExchangeOnlineType} -icon $functionGroupPNGHTML
@@ -227,6 +226,7 @@ function start-HTMLOutput
         out-logfile -string ("Prcessing HTML: "+$string)
 
         New-HTML -TitleText $groupObjectID -FilePath $functionHTMLFile {
+            new-htmlText -text @(("Hierarchy output for group ID: "+$groupObjectID))
             New-HTMLTree -Checkbox none {
                 New-HTMLTreeChildCounter -Deep -HideZero -HideExpanded
                 New-HTMLTreeNode -title $string -children {New-HTMLTreeChildNodes -node $node -outputType $functionLDAPType} -icon $functionGroupPNGHTML
