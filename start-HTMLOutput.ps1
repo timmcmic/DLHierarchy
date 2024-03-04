9a2fb0ef3399496d800d5403e7aea9dcd2e8dfd6
Timothy McMichael <timmcmic@microsoft.com>
2024-03-04 10:58:07 -0500

    Update

--- start-HTMLOutput.ps1
+++ start-HTMLOutput.ps1
@@ -235,7 +235,7 @@ function start-HTMLOutput
                 } -EnableChildCounter -AutoScroll -MinimumExpandLevel 1 -EnableQuickSearch
             } -HeaderTextAlignment "Left" -HeaderTextSize "16" -HeaderTextColor "White" -HeaderBackGroundColor "Black"  -CanCollapse -BorderRadius 10px
             new-htmlSection -HeaderText ("Group membership table for group object id: "+$groupObjectID){
-                new-htmlTable -DataTable (($global:ldapObjects | select-object CanonicalName,distinguishedName,groupType,mail,mailnickanme,objectGUID,proxyAddresses) | sort-object distinguishedName -Unique) -Filtering {
+                new-htmlTable -DataTable (($global:ldapObjects | select-object DistinguishedName,CanonicalName,objectGUID,Name,DisplayName,legacyExchangeDN,groupType,mail,mailnickanme,proxyAddresses,legacyExchangeDN) | sort-object distinguishedName -Unique) -Filtering {
                 } -AutoSize
             } -HeaderTextAlignment "Left" -HeaderTextSize "16" -HeaderTextColor "White" -HeaderBackGroundColor "Black"  -CanCollapse -BorderRadius 10px
         } -Online -ShowHTML 
